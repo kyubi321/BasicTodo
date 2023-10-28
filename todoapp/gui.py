a6392cf6aff1457f32ac9a64f465ef912c57f174
amar <amarnathkco7@gmail.com>
2023-10-28 14:55:22 +0530

    initial commit

--- todoapp/gui.py
+++ todoapp/gui.py
@@ -13,4 +13,4 @@ button = pg.Button("Add")
 # the window of todoapp, as you put values inside the layout put it in the list such a way that elements together must be kept together
 window = pg.Window("my to do app", layout=[[[label], input_box,button],])
 window.read()
-window.close()
+window.close() 
