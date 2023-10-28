2f17f68aa3f36b1c94252b1e02f5b8c164572257
amar <amarnathkco7@gmail.com>
2023-10-28 16:54:09 +0530

    edit button implementation with refresh feature .

--- todoapp/gui.py
+++ todoapp/gui.py
@@ -10,16 +10,68 @@ input_box = pg.InputText(tooltip="enter todo",key="todo")
 # create a button in the todoapp window
 button = pg.Button("Add")
 
+# create a list box to show the list box
+list_box = pg.Listbox(values=ft.read_text(), enable_events=True, size=(45,10),key="todos_list")
+
+# create an edit button to edit the contents in the todos
+edit = pg.Button("Edit")
+
 # the window of todoapp, as you put values inside the layout put it in the list such a way that elements together must be kept together
-window = pg.Window("my to do app", layout=[[[label], input_box,button]],font=("Helvetica",15))
+window = pg.Window("my to do app", layout=[[[label], input_box,button],[list_box,edit]],font=("Helvetica",15))
+
+# to keep the window open until we break the window that is close button.
 while True:
+    # tracking the event and values using two variables
     event, values = window.read()
+
+    # now when each user action leads to value change in events so by using that we access the match case
+
     match event:
+
+        # when the event is add we read the file and append the new data into the file
+        # and this is only done when there is a value in the input box
+
         case "Add":
+            if values['todo'] =='':
+                continue
+            else:
+                todos = ft.read_text()
+                new_todo = values['todo'] + "\n"
+                todos.append(new_todo)
+                ft.write_lines(todos)
+
+                # this code is used to update our listbox in real time
+                window['todos_list'].update(todos)
+
+        # this case is used just to show our selection in the listbox in input box also
+        case "todos_list":
+            window['todo'].update(values['todos_list'][0])
+
+        # this case edits the present todolist
+        case "Edit":
+
+            # we read the file and retrive the list
             todos = ft.read_text()
-            new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
+
+            # then select the value to edit from list box
+            edit_todo = values['todos_list'][0]
+
+            # take the index
+            index = todos.index(edit_todo)
+
+            # retrieve the value that is used to updata
+            update_todo = values['todo'] + '\n'
+
+            # update the todos list
+            todos[index] = update_todo
+
+            # write the todos list in the app
             ft.write_lines(todos)
+
+            # update the list box
+            window['todos_list'].update(todos)
+
+        # this case is used to close the window if user clicks the close button.
         case pg.WIN_CLOSED:
             break
     print(event)
